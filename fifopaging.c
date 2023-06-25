2b682e3c1068e723903620a21de6edfc4f38a930
Govind M H <119432196+g7vind@users.noreply.github.com>
2023-06-25 15:21:16 +0530

    Update fifopaging.c

--- fifopaging.c
+++ fifopaging.c
@@ -1,53 +1,43 @@
-#include<stdio.h>
+// c program to implement fifo page replacement
+#include <stdio.h>
 void main()
 {
-	int i,j,fn,pn,ref[50],frame[50],page_fault=0,victim=-1,flag;
-	printf("Enter the number of frames : ");
-	scanf("%d",&fn);
-	printf("Enter the number of pages : ");
-	scanf("%d",&pn);
-	printf("\n------------------Enter reference string-----------------------\n");
-	for(i=0;i<pn;i++)
-	{
-		scanf("%d",&ref[i]);
-	}
-	printf("\n\n");
-	printf("\n----------------FIFO page replacement--------------\n");
-	for(i=0;i<fn;i++)
-	{
-		frame[i]=-1;
-	}
-	for(i=0;i<pn;i++)
-	{
-		flag=0;
-		for(j=0;j<fn;j++)
-		{
-			if(ref[i]==frame[j])
-			{
-				flag=1;
-				break;
-			}
-		}
-		if(flag==0)
-		{
-			page_fault++;
-			victim++;
-			victim=victim%fn;
-			frame[victim]=ref[i];
-			printf("[incoming : %d ] : ",ref[i]);
-			for(j=0;j<fn;j++)
-			{	
-				if(frame[j]!=-1){
-				printf("%d\t",frame[j]);
-				}
-			}
-			printf("\n");
-		}
-		else
-		{
-			printf("[incoming : %d ] : existing\n",ref[i]);
-		}
-	}
-	printf("\nPAGE FAULT : %d",page_fault);
-	printf("\n");
+    int i, j, flag = 0, pf = 0, ref[50], frame[50], nof, nop, next = 0;
+    printf("Enter number of frames : ");
+    scanf("%d", &nof);
+    for (i = 0; i < nof; i++)
+        frame[i] = -1;
+    printf("Enter number of pages : ");
+    scanf("%d", &nop);
+    printf("Enter page numbers : ");
+    for (i = 0; i < nop; i++)
+        scanf("%d", &ref[i]);
+    for (i = 0; i < nop; i++)
+    {
+        printf("\n");
+        flag = 0;
+        for (j = 0; j < nof; j++)
+        {
+            if (frame[j] == ref[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            frame[next] = ref[i];
+            next = (next + 1) % nof;
+            pf++;
+        }
+        printf("Page no -> ");
+        for (j = 0; j < nof; j++)
+        {
+            if (frame[j] != -1)
+            {
+                printf("%d   ", frame[j]);
+            }
+        }
+    }
+    printf("\n\nPage Fault : %d\n", pf);
 }
